d309bcbd548ed9b8be60e5d48940927b7670da5d
Miguel Carvajal <miguel.carvajal@armadilloamarillo.com>
2019-06-11 12:07:47 +0200

    Updates

---
+++ precache-manifest.d693c999c778707584caa88cb1c0e977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1f00ef7bdd72657584a5cefbd55ebe",
+    "url": "/fileInfo/index.html"
+  },
+  {
+    "revision": "75d249f8cd6ceb9fc370",
+    "url": "/fileInfo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "71e2270de7b37104905d",
+    "url": "/fileInfo/static/js/2.4f531844.chunk.js"
+  },
+  {
+    "revision": "75d249f8cd6ceb9fc370",
+    "url": "/fileInfo/static/js/main.f43aba71.chunk.js"
+  },
+  {
+    "revision": "b41b025e82fe9b2620ad",
+    "url": "/fileInfo/static/js/runtime~main.ca44a226.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fileInfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
